5ee46aec815aaf60468a5b7ddc22768a1ed76658
Christoph Menzel <git@menzel-it.de>
2022-01-08 13:45:38 +0100

    Update layout component
    App changes background according to scroll position
    Use different header images
    Fix warnings

--- src/components/layout.js
+++ src/components/layout.js
@@ -1,56 +1,75 @@
-import { Favorite, Instagram, LinkedIn, Twitter } from '@mui/icons-material';
-import { AppBar, Container, Hidden, Paper, Toolbar, Typography } from '@mui/material';
+import { Favorite, GitHub, Instagram, LinkedIn, Twitter } from '@mui/icons-material';
+import { AppBar, Box, Container, Paper, Toolbar, Typography } from '@mui/material';
 import BottomNavigation from '@mui/material/BottomNavigation';
 import { Link } from 'gatsby';
-import * as React from 'react';
+import React, { useCallback, useEffect, useState } from 'react';
 import HeaderButton from './headerButton';
 import * as containerStyles from './layout.module.css';
 
 const Layout = ({ pageTitle, children }) => {
+    const [customColor, setCustomColor] = useState(false);
+
+    const handleScroll = useCallback(() => {
+        console.log(window.scrollY);
+        if (window.scrollY >= 62) {
+            setCustomColor(true)
+        } else {
+            setCustomColor(false)
+        }
+    }, [setCustomColor]);
+
+    useEffect(() => {
+        window.addEventListener('scroll', handleScroll);
+    }, [handleScroll]);
 
     return (
         <div className={containerStyles.container}>
             <title>{pageTitle}</title>
-            <AppBar>
+            <AppBar sx={{ backgroundColor: customColor ? 'primary.main' : 'transparent', boxShadow: customColor ? '3' : '0' }}>
                 <Container maxWidth="xl">
                     <Toolbar>
                         <Typography sx={{ flexGrow: 1 }}>Traveling Developer Blog</Typography>
-                        <Hidden>
-                            <HeaderButton>test</HeaderButton>
+                        <Box sx={{ display: { xs: 'none', md: 'block' } }}>
                             <HeaderButton>
-                                <Link to="/blog">blog</Link>
+                                <Link to="/">Home</Link>
                             </HeaderButton>
                             <HeaderButton>
-                                <Link to="https://www.instagram.com" className={containerStyles.iconLink}>
+                                <a href="https://www.menzel-it.de">About me</a>
+                            </HeaderButton>
+                            <HeaderButton>
+                                <a href="https://www.instagram.com/traveling_developer_42" className={containerStyles.iconLink}>
                                     <Instagram />
-                                </Link>
+                                </a>
                             </HeaderButton>
                             <HeaderButton>
-                                <Link to="https://www.twitter.com" className={containerStyles.iconLink}>
+                                <a href="https://twitter.com/menzel42" className={containerStyles.iconLink}>
                                     <Twitter />
-                                </Link>
+                                </a>
                             </HeaderButton>
                             <HeaderButton>
-                                <Link to="https://www.linkedin.com" className={containerStyles.iconLink}>
-                                    <LinkedIn />
-                                </Link>
+                                <a href="https://github.com/traveling-developer" className={containerStyles.iconLink}>
+                                    <GitHub />
+                                </a>
                             </HeaderButton>
-                        </Hidden>
+                            <HeaderButton>
+                                <a href="https://www.linkedin.com/in/christoph-menzel/" className={containerStyles.iconLink}>
+                                    <LinkedIn />
+                                </a>
+                            </HeaderButton>
+                        </Box>
                     </Toolbar>
                 </Container>
             </AppBar>
+            <div className={containerStyles.imageHeader} />
             <Paper elevation={10} className={containerStyles.content}>
                 {children}
-
             </Paper>
-            <main >
-            </main>
-            <BottomNavigation sx={{ backgroundColor: 'secondary.main', boxShadow: 3 }} >
+            <BottomNavigation sx={{ backgroundColor: 'transparent' }} >
                 <Container maxWidth="xl" sx={{ display: 'flex', flexDirection: 'row', alignItems: 'center', justifyContent: 'space-evenly' }}>
                     <HeaderButton>
                         <Link to="/imprint">Imprint</Link>
                     </HeaderButton>
-                    <Typography>Made with <Favorite fontSize='small' /> by Traveling Developer </Typography>
+                    <Typography sx={{ display: { xs: 'none', sm: 'block' } }}>Made with <Favorite fontSize='small' sx={{ position: 'relative', top: '3px', height: '20px', color: 'primary.main' }} /> by Traveling Developer </Typography>
                     <HeaderButton>
                         <Link to="/privacy">Privacy</Link>
                     </HeaderButton>
